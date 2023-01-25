ecc8c74d2a0a2b10fa4a5e5d0972e0f2bc64a3c0
veerpa03 <95025440+veerpa03@users.noreply.github.com>
2023-01-25 18:03:50 -0500

    Create git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <viraj parmar>
+ *        Date: <25th jan 2023>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
